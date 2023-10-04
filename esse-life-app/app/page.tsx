a3bfda65c6def9627cd332185dfaa34df22c6369
Filiz Salnur <filizsalnur@gmail.com>
2023-10-04 11:46:37 +0300

    first commit

---
+++ esse-life-app/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <main >
+      <div >
+
+          <button type="button" className="btn btn-primary">Primary</button>
+      </div>
+    </main>
+  )
+}
